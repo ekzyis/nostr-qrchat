4a4738edc39bdd883d3056c6703058a5ae544024
ekzyis <ekzyis@ekzyis.com>
2023-05-09 12:45:43 +0200

    Show npub to user

--- public/index.js
+++ public/index.js
@@ -1,5 +1,9 @@
 console.log(`Your session keys:\n${nsec} ${npub}`);
 
+for (const el of document.querySelectorAll(".npub")) {
+  el.textContent = npub.slice(0, 10) + ".." + npub.slice(-10);
+}
+
 document.getElementById("chat-form").addEventListener("submit", (event) => {
   event.preventDefault();
   const form = event.target;
