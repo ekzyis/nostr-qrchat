a5ef61d7ca0348c6b2e92fe09fdb4a5b95c6f308
ekzyis <ekzyis@ekzyis.com>
2023-05-10 10:45:09 +0200

    Add tokens

--- main.go
+++ main.go
@@ -1,62 +1,121 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
+	"strings"
 )
+
+type Token struct {
+	Name  string
+	Value string
+}
 
 var (
-	NsecSessionKeyRegexp          = regexp.MustCompile(regexp.QuoteMeta(`"" /** NSEC SESSION KEY **/`))
-	NpubSessionKeyRegexp          = regexp.MustCompile(regexp.QuoteMeta(`"" /** NPUB SESSION KEY **/`))
-	NpubRecipientSessionKeyRegexp = regexp.MustCompile(regexp.QuoteMeta(`"" /** NPUB RECIPIENT KEY **/`))
+	StaticServeDir                = "public"
+	Tokens                        []Token
+	NsecSessionKeyRegexp          = regexp.MustCompile(regexp.QuoteMeta(`""; /** NSEC SESSION KEY **/`))
+	NpubSessionKeyRegexp          = regexp.MustCompile(regexp.QuoteMeta(`""; /** NPUB SESSION KEY **/`))
+	NpubRecipientSessionKeyRegexp = regexp.MustCompile(regexp.QuoteMeta(`""; /** NPUB RECIPIENT KEY **/`))
 )
 
-func generateSession() (string, error) {
-	content, err := ioutil.ReadFile("public/index.html")
+func init() {
+	file, err := os.Open("tokens")
 	if err != nil {
-		return "", fmt.Errorf("error reading index.html: %w", err)
+		log.Fatal("error reading tokens: ", err)
 	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ":")
+		name := ""
+		token := parts[0]
+		if len(parts) > 1 {
+			name = parts[0]
+			token = parts[1]
+		}
+		Tokens = append(Tokens, Token{name, token})
+	}
+}
 
+func addSession(indexFile []byte) (string, error) {
 	keys, err := GenerateKeyPair()
 	if err != nil {
 		return "", fmt.Errorf("error generating session keys: %w", err)
 	}
 	privKey, pubKey := keys[0], keys[1]
 
-	modifiedContent := NsecSessionKeyRegexp.ReplaceAllString(string(content), fmt.Sprintf(`"%s"`, privKey))
-	modifiedContent = NpubSessionKeyRegexp.ReplaceAllString(modifiedContent, fmt.Sprintf(`"%s"`, pubKey))
-	modifiedContent = NpubRecipientSessionKeyRegexp.ReplaceAllString(modifiedContent, fmt.Sprintf(`"%s"`, NostrPubKey))
+	sessionFile := NsecSessionKeyRegexp.ReplaceAllString(string(indexFile), fmt.Sprintf(`"%s"`, privKey))
+	sessionFile = NpubSessionKeyRegexp.ReplaceAllString(sessionFile, fmt.Sprintf(`"%s"`, pubKey))
+	sessionFile = NpubRecipientSessionKeyRegexp.ReplaceAllString(sessionFile, fmt.Sprintf(`"%s"`, NostrPubKey))
 
-	return modifiedContent, nil
+	return sessionFile, nil
 }
 
-func fileHandler(h http.Handler) http.Handler {
+func loadIndexFile() ([]byte, error) {
+	content, err := ioutil.ReadFile("public/index.html")
+	if err != nil {
+		return []byte{}, fmt.Errorf("error reading index.html: %w", err)
+	}
+	return content, nil
+}
+
+func handler(static http.Handler, session http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/" {
-			// intercept the request for the index.html file
-			// to generate a new session
-			indexFile, err := generateSession()
-			if err != nil {
-				log.Println(err)
-				http.Error(w, "Failed to generate session", http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "text/html; charset=utf-8")
-			w.Write([]byte(indexFile))
+		// try to serve static files
+		_, err := http.Dir("public").Open(r.URL.Path)
+		if err == nil {
+			static.ServeHTTP(w, r)
 			return
 		}
-
-		// all other requests are passed to the underlying file server
-		h.ServeHTTP(w, r)
+		// else check if URL uses valid session token
+		session.ServeHTTP(w, r)
 	})
 }
 
+func checkToken(token string) bool {
+	for _, token_ := range Tokens {
+		if token_.Value == token {
+			return true
+		}
+	}
+	return false
+}
+
+func sessionHandler(w http.ResponseWriter, r *http.Request) {
+	indexFile, err := loadIndexFile()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "error reading index.html", http.StatusInternalServerError)
+		return
+	}
+	token := strings.TrimLeft(r.URL.Path, "/")
+	valid := checkToken(token)
+	if !valid {
+		// Send index.html without session
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write(indexFile)
+		return
+	}
+	sessionFile, err := addSession(indexFile)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "error generating session", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write([]byte(sessionFile))
+}
+
 func main() {
-	fs := http.FileServer(http.Dir("public"))
-	http.Handle("/", fileHandler(fs))
+	fs := http.FileServer(http.Dir(StaticServeDir))
+	http.Handle("/", handler(fs, http.HandlerFunc(sessionHandler)))
 
 	log.Println("Server started on http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
--- public/index.js
+++ public/index.js
@@ -1,122 +1,136 @@
-console.log(`Your session keys:\n${nsec} ${npub}`);
-console.log(`You are writing to:\n${npubRecipient}`);
+const validSession = !!nsec && !!npub && !!npubRecipient;
 
-const { data: sk } = NostrTools.nip19.decode(nsec);
-const { data: pk1 } = NostrTools.nip19.decode(npub);
-const { data: pk2 } = NostrTools.nip19.decode(npubRecipient);
-
-const formatKey = (key) => key.slice(0, 10) + ".." + key.slice(-10);
-
-for (const el of document.querySelectorAll(".npub")) {
-  el.textContent = formatKey(npub);
-}
-for (const el of document.querySelectorAll(".npubRecipient")) {
-  el.textContent = formatKey(npubRecipient);
-}
-
-const history = document.getElementById("history");
 const errorEl = document.getElementById("error");
 
-const relays = [
-  "wss://relay.damus.io/",
-  // "wss://nostr-pub.wellorder.net/",
-  "wss://relay.snort.social/",
-  "wss://relay.nostr.band/",
-  "wss://nostr.wine/",
-  "wss://nostr.mutinywallet.com/",
-  "wss://nostr.bitcoiner.social/",
-  // "wss://rsslay.fiatjaf.com/",
-  "wss://nostr.rocks/",
-];
-const pool = new NostrTools.SimplePool();
-const sub = pool.sub(relays, [
-  {
-    kinds: [4],
-    "#p": [pk1],
-  },
-]);
+function initSession() {
+  console.log(`Your session keys:\n${nsec} ${npub}`);
+  console.log(`You are writing to:\n${npubRecipient}`);
 
-async function decryptDM(content) {
-  return NostrTools.nip04.decrypt(sk, pk2, content);
-}
+  const { data: sk } = NostrTools.nip19.decode(nsec);
+  const { data: pk1 } = NostrTools.nip19.decode(npub);
+  const { data: pk2 } = NostrTools.nip19.decode(npubRecipient);
 
-function addMessageToHistory(msg, pubkey) {
-  const recv = pubkey === pk2;
-  const bubble = document.createElement("div");
-  bubble.classList.add("chat-bubble");
-  bubble.classList.add(recv ? "recv" : "send");
-  const text = document.createElement("div");
-  text.classList.add("chat-text");
-  text.textContent = msg;
-  bubble.appendChild(text);
-  history.appendChild(bubble);
-}
+  const formatKey = (key) => key.slice(0, 10) + ".." + key.slice(-10);
 
-sub.on("event", async (event) => {
-  const msg = await decryptDM(event.content);
-  console.log(`Received msg: content=${msg} id=${event.id}`);
-  addMessageToHistory(msg, event.pubkey);
-});
-
-async function createEncryptedDM(msg) {
-  const ciphertext = await NostrTools.nip04.encrypt(sk, pk2, msg);
-
-  let event = {
-    kind: 4,
-    pubkey: pk1,
-    tags: [["p", pk2]],
-    content: ciphertext,
-    created_at: Math.floor(Date.now() / 1000),
-  };
-  event.id = NostrTools.getEventHash(event);
-  event.sig = NostrTools.signEvent(event, sk);
-
-  let ok = NostrTools.validateEvent(event);
-  if (!ok) {
-    throw new Error("event validation failed");
+  for (const el of document.querySelectorAll(".npub")) {
+    el.textContent = formatKey(npub);
   }
-  let sigOk = NostrTools.verifySignature(event);
-  if (!sigOk) {
-    throw new Error("signature verification failed");
+  for (const el of document.querySelectorAll(".npubRecipient")) {
+    el.textContent = formatKey(npubRecipient);
   }
-  return event;
+
+  const history = document.getElementById("history");
+
+  const relays = [
+    "wss://relay.damus.io/",
+    // "wss://nostr-pub.wellorder.net/",
+    "wss://relay.snort.social/",
+    "wss://relay.nostr.band/",
+    "wss://nostr.wine/",
+    "wss://nostr.mutinywallet.com/",
+    "wss://nostr.bitcoiner.social/",
+    // "wss://rsslay.fiatjaf.com/",
+    "wss://nostr.rocks/",
+  ];
+  const pool = new NostrTools.SimplePool();
+  const sub = pool.sub(relays, [
+    {
+      kinds: [4],
+      "#p": [pk1],
+    },
+  ]);
+
+  async function decryptDM(content) {
+    return NostrTools.nip04.decrypt(sk, pk2, content);
+  }
+
+  function addMessageToHistory(msg, pubkey) {
+    const recv = pubkey === pk2;
+    const bubble = document.createElement("div");
+    bubble.classList.add("chat-bubble");
+    bubble.classList.add(recv ? "recv" : "send");
+    const text = document.createElement("div");
+    text.classList.add("chat-text");
+    text.textContent = msg;
+    bubble.appendChild(text);
+    history.appendChild(bubble);
+  }
+
+  sub.on("event", async (event) => {
+    const msg = await decryptDM(event.content);
+    console.log(`Received msg: content=${msg} id=${event.id}`);
+    addMessageToHistory(msg, event.pubkey);
+  });
+
+  async function createEncryptedDM(msg) {
+    const ciphertext = await NostrTools.nip04.encrypt(sk, pk2, msg);
+
+    let event = {
+      kind: 4,
+      pubkey: pk1,
+      tags: [["p", pk2]],
+      content: ciphertext,
+      created_at: Math.floor(Date.now() / 1000),
+    };
+    event.id = NostrTools.getEventHash(event);
+    event.sig = NostrTools.signEvent(event, sk);
+
+    let ok = NostrTools.validateEvent(event);
+    if (!ok) {
+      throw new Error("event validation failed");
+    }
+    let sigOk = NostrTools.verifySignature(event);
+    if (!sigOk) {
+      throw new Error("signature verification failed");
+    }
+    return event;
+  }
+
+  async function sendEncryptedDM(msg) {
+    const event = await createEncryptedDM(msg);
+    console.log(`Sending msg: content=${msg} id=${event.id}`);
+    addMessageToHistory(msg, pk1);
+    const pubs = pool.publish(relays, event);
+    pubs.on("ok", () => {
+      // this may be called multiple times, once for every relay that accepts the event
+      console.log(`Sending msg: content=${msg} id=${event.id} - OK`);
+    });
+  }
+
+  document
+    .getElementById("chat-form")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      const form = event.target;
+      const msg = form.elements["msg"].value;
+      if (!msg) return;
+      await sendEncryptedDM(msg)
+        .then(() => {
+          errorEl.textContent = "";
+        })
+        .catch((err) => {
+          errorEl.textContent = err;
+        });
+
+      form.reset();
+    });
+
+  setTimeout(() => {
+    // notify recipient about new session
+    createEncryptedDM("new QRchat session").then((event) => {
+      pool.publish(relays, event);
+    });
+    const prompt = "Hi. Who's there?";
+    addMessageToHistory(prompt, pk2);
+  }, 3000);
 }
 
-async function sendEncryptedDM(msg) {
-  const event = await createEncryptedDM(msg);
-  console.log(`Sending msg: content=${msg} id=${event.id}`);
-  addMessageToHistory(msg, pk1);
-  const pubs = pool.publish(relays, event);
-  pubs.on("ok", () => {
-    // this may be called multiple times, once for every relay that accepts the event
-    console.log(`Sending msg: content=${msg} id=${event.id} - OK`);
-  });
+if (validSession) {
+  initSession();
+} else {
+  for (const el of document.querySelectorAll(".session")) {
+    el.style.display = "none";
+  }
+  errorEl.textContent = "session not found";
 }
-
-document
-  .getElementById("chat-form")
-  .addEventListener("submit", async (event) => {
-    event.preventDefault();
-
-    const form = event.target;
-    const msg = form.elements["msg"].value;
-    if (!msg) return;
-    await sendEncryptedDM(msg)
-      .then(() => {
-        errorEl.textContent = "";
-      })
-      .catch((err) => {
-        errorEl.textContent = err;
-      });
-
-    form.reset();
-  });
-
-setTimeout(() => {
-  // notify recipient about new session
-  createEncryptedDM("new QRchat session").then((event) => {
-    pool.publish(relays, event);
-  });
-  const prompt = "Hi. Who's there?";
-  addMessageToHistory(prompt, pk2);
-}, 3000);
